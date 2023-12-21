0ef2a8b6244ad00b06e708cec31d0164609b286d
daniildeli <daniildeli@gmail.com>
2023-12-21 01:05:42 +0100

    Added context

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,7 +4,6 @@ export default function Shop({ children, }) {
   return (
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
-
       <ul id="products">
         {children}
       </ul>
